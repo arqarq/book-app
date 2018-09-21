7595969fafa45591a1da0af2c8349dbfba34a311
arqarq <43341071+arqarq@users.noreply.github.com>
2018-09-21 09:23:01 +0200

    ostateczna synchronizacja na dwa kompy

--- src/main/java/pl/sda/bookApp/Book.java
+++ src/main/java/pl/sda/bookApp/Book.java
@@ -5,17 +5,17 @@ class Book {
     private final String author;
     private final int reldate;
     private final int pagecount;
-    private final String publisherrr;
+    private final String publisher;
     private final String type;
     private final String ISBN;
     private final int linenumberinfile;
 
-    Book(String title, String author, int reldate, int pagecount, String publisherrr, String type, String ISBN, int linenumberinfile) {
+    Book(String title, String author, int reldate, int pagecount, String publisher, String type, String ISBN, int linenumberinfile) {
         this.title = title;
         this.author = author;
         this.reldate = reldate;
         this.pagecount = pagecount;
-        this.publisherrr = publisherrr;
+        this.publisher = publisher;
         this.type = type;
         this.ISBN = ISBN;
         this.linenumberinfile = linenumberinfile;
